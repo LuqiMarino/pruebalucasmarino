debf5b95959317b3215e8604bd1007143c0d1852
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-03 17:48:13 -0300

    commit989

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent {
   title = 'pruebalucasmarino';
 
   //esto no va aca
-  public edad1 = 0;
-  public edad2 = 0;
-  public prom = 0;
+  // public edad1 = 0;
+  // public edad2 = 0;
+  // public prom = 0;
 
-  public CalcularPromedio():void{    
-    this.edad1 = parseInt((<HTMLInputElement>document.getElementById("edad1")).value);
-    this.edad2 = parseInt((<HTMLInputElement>document.getElementById("edad2")).value);
-    this.prom = (this.edad1+this.edad2) / 2;
-  }
+  // public CalcularPromedio():void{    
+  //   this.edad1 = parseInt((<HTMLInputElement>document.getElementById("edad1")).value);
+  //   this.edad2 = parseInt((<HTMLInputElement>document.getElementById("edad2")).value);
+  //   this.prom = (this.edad1+this.edad2) / 2;
+  // }
 
-  public Limpiar():void{    
-    (<HTMLInputElement>document.getElementById("edad1")).value = "";
-    (<HTMLInputElement>document.getElementById("edad2")).value = "";
-    (<HTMLInputElement>document.getElementById("prom")).value = "";
-  }
+  // public Limpiar():void{    
+  //   (<HTMLInputElement>document.getElementById("edad1")).value = "";
+  //   (<HTMLInputElement>document.getElementById("edad2")).value = "";
+  //   (<HTMLInputElement>document.getElementById("prom")).value = "";
+  // }
 }
