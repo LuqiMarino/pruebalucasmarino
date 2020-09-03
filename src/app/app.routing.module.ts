6a159060102846dfb654cebfaaca817b52dfa974
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-03 15:49:26 -0300

    commit2

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,13 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Clase1Component } from './Ejercicio/clase1/clase1.component';
 
-// const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Clase1Component},
+];
 
-// @NgModule({
-//     imports: [RouterModule.forRoot(routes)],
-//     exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
