06ede6450ada1951d843cd80862fcbb7bc2bca7d
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-03 18:03:49 -0300

    commit989

--- src/app/Ejercicio/clase1/clase1.component.ts
+++ src/app/Ejercicio/clase1/clase1.component.ts
@@ -15,17 +15,20 @@ export class Clase1Component implements OnInit {
   public edad1 = 0;
   public edad2 = 0;
   public prom = 0;
+  public suma = 0;
 
   public CalcularPromedio():void{    
     this.edad1 = parseInt((<HTMLInputElement>document.getElementById("edad1")).value);
     this.edad2 = parseInt((<HTMLInputElement>document.getElementById("edad2")).value);
     this.prom = (this.edad1+this.edad2) / 2;
+    this.suma = this.edad1+this.edad2;
   }
 
   public Limpiar():void{    
     (<HTMLInputElement>document.getElementById("edad1")).value = "";
     (<HTMLInputElement>document.getElementById("edad2")).value = "";
     (<HTMLInputElement>document.getElementById("prom")).value = "";
+    (<HTMLInputElement>document.getElementById("suma")).value = "";
   }
 
 }
