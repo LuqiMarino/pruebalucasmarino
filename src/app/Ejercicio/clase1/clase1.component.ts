5f08337271bc71288933f3af34796fa1246055e4
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-03 15:08:56 -0300

    commit2

--- src/app/Ejercicio/clase1/clase1.component.ts
+++ src/app/Ejercicio/clase1/clase1.component.ts
@@ -12,16 +12,20 @@ export class Clase1Component implements OnInit {
   ngOnInit(): void {
   }
 
+  public edad1 = 0;
+  public edad2 = 0;
+  public prom = 0;
+
   public CalcularPromedio():void{    
-    var edad1 = parseInt((<HTMLInputElement>document.getElementById("edad1")).value);
-    var edad2 = parseInt((<HTMLInputElement>document.getElementById("edad2")).value);
-    var prom = (edad1+edad2) / 2;
-    alert(prom);
+    this.edad1 = parseInt((<HTMLInputElement>document.getElementById("edad1")).value);
+    this.edad2 = parseInt((<HTMLInputElement>document.getElementById("edad2")).value);
+    this.prom = (this.edad1+this.edad2) / 2;
   }
 
   public Limpiar():void{    
     (<HTMLInputElement>document.getElementById("edad1")).value = "";
     (<HTMLInputElement>document.getElementById("edad2")).value = "";
+    (<HTMLInputElement>document.getElementById("prom")).value = "";
   }
 
 }
